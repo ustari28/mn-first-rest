7f82ec81d940052b4fd20f9d538d9519553bdae3
Alan <ustargab@gmail.com>
2019-09-29 21:32:09 +0200

    doesn't work

--- src/main/java/mn/first/rest/model/Greeting.java
+++ src/main/java/mn/first/rest/model/Greeting.java
@@ -3,9 +3,16 @@ package mn.first.rest.model;
 import lombok.Builder;
 import lombok.Data;
 
-@Data
 @Builder
 public class Greeting {
 
     private String action;
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
 }
