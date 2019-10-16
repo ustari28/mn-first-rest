bd32c67384c8098e58e0e56f30bf3034af987ee1
ustargab@gmail.com <b24c2bd99ad04a7e98daa526aa4be475>
2019-10-16 17:31:08 +0200

    docker

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-docker build . -t mn-first-rest
+docker build . -t mn-first-rest:1.0.0
 echo
 echo
 echo "To run the docker container execute:"
-echo "    $ docker run -p 8080:8080 mn-first-rest"
+echo "    $ docker run -p 8080:8080 mn-first-rest:1.0.0"
+echo "    $ docker run -d -p 8080:8080 -e EXTERNAL_PORT= -e EUREKA_HOST=eureka-server --network BALANCER --name mn-first-rest-1 mn-first-rest:1.0.0"
+
